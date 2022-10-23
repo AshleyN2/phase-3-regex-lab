5581ddbeb2ce3d9da920f2c71544d90b4987c1f3
Ashley <ashleynjeri7@gmail.com>
2022-10-23 14:10:28 +0300

    first commit

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,16 @@
 def starts_with_a_vowel?(word)
-
+    if
+        word = word.scan(/\b[AEIOUaeiou][a-z]*\b/)
+        true
+    else
+        word = word.scan(/\b[^AEIOUaeiou][a-z]*\b/)
+        false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text = text.scan(/\w +^un + ing$/)
+    text
 end
 
 def words_five_letters_long(text)
